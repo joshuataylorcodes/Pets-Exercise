a2afa223d7ab412155d8793fb73051e8e732b098
joshua <joshuamtaylor455@gmail.com>
2023-03-20 16:14:57 -0400

    create the ability to create user and create session that user create by logging in

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   root "pets#index"
 
   resources :pets
+
+  #this is for users(creating new users)
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  #this is for creating a session for a user
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+
+  
 end
