b3e557703df267866751427f906d758df9df41c8
joshua <joshuamtaylor455@gmail.com>
2023-03-20 17:11:36 -0400

    add user authentication so user can only see the pets created by user. Add code so only current user can create/destroy pets

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,8 @@
 class PetsController < ApplicationController
+  before_action :authenticate_user
+
   def index
-    @pets = Pet.all
+    @pets = current_user.pets.where(user_id: current_user.id)
   end
 
   def show
@@ -28,7 +30,6 @@ class PetsController < ApplicationController
 
   def update
     @pet = Pet.find(params[:id])
-    @pet.user_id = current_user.id
 
     if @pet.update(pet_params)
       redirect_to @pet
@@ -38,7 +39,8 @@ class PetsController < ApplicationController
   end
 
   def destroy
-    @pet = Pet.find(params[:id])
+    # @pet = Pet.find(params[:id])
+    @pet = current_user.pets.find(params[:id])
     @pet.destroy
 
     redirect_to root_path, status: :see_other
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,4 +3,5 @@ class Pet < ApplicationRecord
   validates :name, presence: true
   validates :breed, presence: true, length: { minimum: 3 }
   validates :image, presence: true
+  validates :user_id, presence: true
 end
