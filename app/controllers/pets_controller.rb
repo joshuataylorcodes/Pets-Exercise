1aaef17c2485d1e6177892992d8f99241f540dcf
joshua <joshuamtaylor455@gmail.com>
2023-03-20 15:04:06 -0400

    create Pet app(controller,database,model), add index, show, create actions using ERB to disply in our local host

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,29 @@
+class PetsController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+
+    if @pet.save
+      redirect_to @pet
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :breed, :image)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root "pets#index"
+
+  resources :pets
+end
